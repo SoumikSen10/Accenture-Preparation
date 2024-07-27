896b0771c7e45ae9704b8005fdd8db435363586f
Soumik Sen <mastersoumik2003@gmail.com>
2024-07-28 04:55:13 +0530

    Adding all files

---
+++ Accenture Coding Prep/src/EquilibriumPoint.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class EquilibriumPoint
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter length of array: ");
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        System.out.print("Enter the array elements: ");
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=in.nextInt();
+        }
+        System.out.print("The array elements are: ");
+        System.out.println(arr.toString());
+
+        int x = equilibriumPoint(arr);
+        System.out.println(x);
+    }
+
+    public static int equilibriumPoint(int[] arr)
+    {
+        int n=arr.length;
+        int sum=0,prefixSum=0;
+        for(int i=0;i<n;i++)
+            sum+=arr[i];
+        for(int i=0;i<n;i++)
+        {
+            sum-=arr[i];
+            if(sum==prefixSum)
+                return i;
+            prefixSum+=arr[i];
+        }
+        return -1;
+    }
+}
---
+++ Accenture Coding Prep/src/LongestString.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LongestString
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s=in.nextLine();
+        String s1=findLongest(s);
+        System.out.println(s1);
+    }
+    public static String findLongest(String s)
+    {
+        if(s.equals(""))
+            return "";
+        String[] str = s.split(" ");
+        int maxLen=Integer.MIN_VALUE;
+        String maxStr="";
+        for(String t : str)
+        {
+            if(t.length()>maxLen) {
+                maxLen=t.length();
+                maxStr = t;
+            }
+        }
+        return maxStr;
+    }
+}
